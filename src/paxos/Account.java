b2f6cee1a5c030926acfa5d312434d9c2e9ea411
bansal <akankshabansal90@gmail.com>
2013-11-05 18:03:43 -0600

    Account class

---
+++ src/paxos/Account.java
@@ -0,0 +1,42 @@
+package paxos;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bansal
+ * Date: 05/11/13
+ * Time: 5:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Account {
+    int accountNo;
+    int balance;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int debit(int by) {
+        return (this.balance = balance - by );
+    }
+    public int credit(int by) {
+       return (this.balance = balance + by );
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNo=" + accountNo +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public Account(String str, int accountNo){
+        this.accountNo= accountNo;
+        this.balance = Integer.parseInt(str.split(Env.TX_MSG_SEPARATOR)[1]);
+    }
+
+}
